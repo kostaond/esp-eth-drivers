4fdd471586795b55628adf525ca5fbd9e466b467
Ondrej Kosta <ondrej.kosta@espressif.com>
2025-03-19 11:29:09 +0100

    ci: added tag annotation to tag created by cz

--- tools/changelog.py
+++ tools/changelog.py
@@ -12,7 +12,7 @@ import sys
 from typing import Dict
 from typing import List
 
-RELEASE_TAG_BASE_URL = 'https://github.com/kostaond/esp-eth-drivers/releases/tag'
+RELEASE_TAG_BASE_URL = 'https://github.com/kostaond/esp-eth-drivers/commits/' # TODO change to espressif
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parents[1]
 CZ_OLD_TAG = os.environ['CZ_PRE_CURRENT_TAG_VERSION']
 CZ_NEW_TAG = os.environ['CZ_PRE_NEW_TAG_VERSION']
@@ -51,8 +51,37 @@ def check_repo() -> None:
         assert CZ_OLD_TAG in ref_tags
 
 
-def update_changelog(component:str) -> None:
+def update_changelog(component:str, release_notes:str) -> None:
     """Update Changelog file from git history"""
+    changelog_path = str(PROJECT_ROOT / component / 'CHANGELOG.md')
+    # Update changelog file
+    changelog_data: List[str]
+    try:
+        with open(changelog_path, encoding='utf-8') as fr:
+            changelog_data = fr.readlines()
+    except FileNotFoundError:
+        changelog_data = [f'# Changelog - {component}\n\n', '']
+    changelog_data.insert(2, ''.join(release_notes))
+    with open(changelog_path, 'w', encoding='utf-8') as fw:
+        fw.write(''.join(changelog_data))
+    # Stage updated changelog to be committed along with version bump
+    subprocess.check_call(['git', 'add', changelog_path],
+                          cwd=PROJECT_ROOT)
+
+
+def create_release_notes(component:str, release_notes:str) -> None:
+    """Format release notes"""
+    # remove markdown links and formatting
+    release_notes = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', release_notes)
+    release_notes = re.sub(r'\#\#[\#\s]*(.+)', r'\1', release_notes)
+    release_notes = re.sub(r'\n\n', '\n', release_notes)
+    # store release notes to a temp file to be used later in the release process
+    with open(str(PROJECT_ROOT / component / '_release_notes.txt'), 'w', encoding='utf-8') as fw:
+        fw.write(release_notes)
+
+
+def get_commit_changes(component:str) -> str:
+    """Get and markdown format git history"""
     # Update ChangeLog
     if CZ_INITIAL == 'True':
         git_logs = subprocess.check_output(
@@ -74,35 +103,31 @@ def update_changelog(component:str) -> None:
         assert _commit_match
         commit = _commit_match.group(0)
         for match in CHANGELOG_PATTERN.finditer(commit_log):
-            if match.group(2):
+            if match.group(2) == component:
                 _changelog = f'- {match.group(2)}: {match.group(3)} ([{commit}]({RELEASE_TAG_BASE_URL}{commit}))'
             else:
+                # TODO commit is not related to the component, expect user action??
                 _changelog = f'- {match.group(3)} ([{commit}]({RELEASE_TAG_BASE_URL}{commit}))'
             changelogs[CHANGELOG_SECTIONS[match.group(1)]].append(_changelog)
 
-    # Update changelog file
-    changelog_data: List[str]
-    file = str(PROJECT_ROOT / component / 'CHANGELOG.md')
-    try:
-        with open(file, encoding='utf-8') as fr:
-            changelog_data = fr.readlines()
-    except FileNotFoundError:
-        changelog_data = [f'# Changelog - {component}\n\n', '']
     changed = False
+    formatted_changes: List[str] = ['']
     for key, values in changelogs.items():
         if not values:
             continue
-        changelog_data.insert(2, f'### {key}\n\n' + '\n'.join(values) + '\n\n')
+        formatted_changes.insert(0, f'### {key}\n\n' + '\n'.join(values) + '\n\n')
         changed = True
     if changed:
-        changelog_data.insert(2, f'## [{CZ_NEW_TAG}]({RELEASE_TAG_BASE_URL}/{CZ_NEW_TAG})\n\n')
-    with open(file, 'w', encoding='utf-8') as fw:
-        fw.write(''.join(changelog_data))
-    subprocess.check_call(['git', 'add', f'{file}'],
-                          cwd=PROJECT_ROOT)
+        formatted_changes.insert(0, f'## [{CZ_NEW_TAG}]({RELEASE_TAG_BASE_URL}/{CZ_NEW_TAG})\n\n')
+    else:
+        raise RuntimeError('No changes found')
+    return ''.join(formatted_changes)
 
 
 if __name__ == '__main__':
     check_repo()
     args = sys.argv[1:]
-    update_changelog(args[0])
+    scope = args[0]
+    CHANGES = get_commit_changes(scope)
+    update_changelog(scope, CHANGES)
+    create_release_notes(scope, CHANGES)
